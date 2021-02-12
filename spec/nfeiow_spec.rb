05de4e72ea82514b61d9101287d0fd51779b1dad
Anchieta Júnior <santosjr87@gmail.com>
2021-02-12 07:48:39 -0300

    Remove smoke test, update default task

--- spec/nfeiow_spec.rb
+++ spec/nfeiow_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Nfeiow do
   it "has a version number" do
     expect(Nfeiow::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(true).to eq(true)
-  end
 end
